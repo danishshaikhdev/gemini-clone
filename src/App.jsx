22b4c9e3f8eed871e0c9d878218cb3be6f15f37c
Danish Shaikh <danishshk0399@gmail.com>
2025-09-18 03:29:58 +0530

    feat: set up React project with Vite and Tailwind CSS
    - Updated package.json to include Vite, React, and Tailwind CSS dependencies.
    - Created main application component (App.jsx) with a simple "Hello World!" message.
    - Added index.css to import Tailwind CSS styles.
    - Implemented main entry point (main.jsx) to render the App component.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold text-red-700 text-shadow-lg'>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
